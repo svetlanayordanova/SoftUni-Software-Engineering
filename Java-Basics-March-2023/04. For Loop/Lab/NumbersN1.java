1d284779773d150b5b2f7474868287e08a263a2e
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2023-09-26 10:34:34 +0300

    Add files via upload

---
+++ Java-Basics-March-2023/04. For Loop/Lab/NumbersN1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class NumbersN1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int number = n; number >= 1; number--) {
+            System.out.println(number);
+        }
+    }
+}
