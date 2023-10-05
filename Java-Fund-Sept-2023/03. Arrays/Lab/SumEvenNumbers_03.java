861c17a009eb1c731a184d2aa4535d196e4fc055
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2023-10-05 09:41:48 +0300

    Add files via upload

---
+++ Java-Fund-Sept-2023/03. Arrays/Lab/SumEvenNumbers_03.java
@@ -0,0 +1,23 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+public class SumEvenNumbers_03 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int sum = 0;
+
+        for (int i = 0; i <= numbers.length - 1; i++) {
+            if (numbers[i] % 2 == 0) {
+                sum += numbers[i];
+            }
+        }
+        System.out.println(sum);
+    }
+}
