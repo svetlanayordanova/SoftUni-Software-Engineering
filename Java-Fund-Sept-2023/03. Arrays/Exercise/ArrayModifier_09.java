8c759b0dd145442fd2991b9b2e3224911fb2d4d0
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2023-10-09 09:23:29 +0300

    Add files via upload

---
+++ Java-Fund-Sept-2023/03. Arrays/Exercise/ArrayModifier_09.java
@@ -0,0 +1,47 @@
+package arrays_exercise;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayModifier_09 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[] array = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        String command = scanner.nextLine();
+
+        while (!command.equals("end")) {
+            if (command.contains("swap")) {
+                int index1 = Integer.parseInt(command.split(" ")[1]);
+                int index2 = Integer.parseInt(command.split(" ")[2]);
+
+                int firstIndexNumber = array[index1];
+                int secondIndexNumber = array[index2];
+
+                array[index1] = secondIndexNumber;
+                array[index2] = firstIndexNumber;
+            } else if (command.contains("multiply")) {
+                int index1 = Integer.parseInt(command.split(" ")[1]);
+                int index2 = Integer.parseInt(command.split(" ")[2]);
+
+                int firstIndexNumber = array[index1];
+                int secondIndexNumber = array[index2];
+                int product = firstIndexNumber * secondIndexNumber;
+
+                array[index1] = product;
+            } else if (command.equals("decrease")) {
+                for (int index = 0; index <= array.length - 1; index++) {
+                    array[index]--;
+                }
+            }
+            command = scanner.nextLine();
+        }
+        System.out.println(Arrays.toString(array)
+                .replace("[", "")
+                .replace("]", ""));
+    }
+}
