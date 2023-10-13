23a32facad6f09c2f0936891bbbf2b82da913e7f
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2023-10-13 08:58:32 +0300

    Add files via upload

---
+++ Java-Fund-Sept-2023/04. Methods/Exercise/CharactersInRange_03.java
@@ -0,0 +1,26 @@
+package methods_exercise;
+
+import java.util.Scanner;
+public class CharactersInRange_03 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        char firstSymbol = scanner.nextLine().charAt(0);
+        char secondSymbol = scanner.nextLine().charAt(0);
+
+        printSymbolsInRange(firstSymbol, secondSymbol);
+    }
+
+    public static void printSymbolsInRange(char firstSymbol, char secondSymbol) {
+        if (firstSymbol < secondSymbol) {
+            for (char symbol = (char) (firstSymbol + 1); symbol < secondSymbol; symbol++) {
+                System.out.print(symbol + " ");
+            }
+        } else {
+            for (char symbol = (char) (secondSymbol + 1); symbol < firstSymbol; symbol++) {
+                System.out.print(symbol + " ");
+            }
+        }
+    }
+}
