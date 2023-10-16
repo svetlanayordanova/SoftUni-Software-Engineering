705002dec8aecbba614e5a3415d68c344110c1ed
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2023-10-16 09:04:18 +0300

    Add files via upload

---
+++ Java-Fund-Sept-2023/04. Methods/Exercise/FactorialDivision_08.java
@@ -0,0 +1,28 @@
+package methods_exercise;
+
+import java.util.Scanner;
+public class FactorialDivision_08 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int firstNumber = Integer.parseInt(scanner.nextLine());
+        int secondNumber = Integer.parseInt(scanner.nextLine());
+
+        long factFirstNumber = calculateFactorial(firstNumber);
+        long factSecondNumber = calculateFactorial(secondNumber);
+
+        double result = factFirstNumber * 1.0 / factSecondNumber;
+
+        System.out.printf("%.2f", result);
+    }
+
+    public static long calculateFactorial(int number) {
+        long fact = 1;
+
+        for (int i = 1; i <= number; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+}
