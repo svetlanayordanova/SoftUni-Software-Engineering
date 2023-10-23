e2861c72e3d59b23c13f82f3c87258ac678f58a8
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2023-10-23 09:32:11 +0300

    Add files via upload

---
+++ Java-Fund-Sept-2023/05. Lists/Exercise/ChangeList_02.java
@@ -0,0 +1,38 @@
+package lists_exercise;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class ChangeList_02 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        String command = scanner.nextLine();
+
+        while (!command.equals("end")) {
+            if (command.contains("Delete")) {
+                int numberForDelete = Integer.parseInt(command.split(" ")[1]);
+
+                numbers.removeAll(Arrays.asList(numberForDelete));
+            } else if (command.contains("Insert")) {
+                int element = Integer.parseInt(command.split(" ")[1]);
+                int position = Integer.parseInt(command.split(" ")[2]);
+
+                numbers.add(position, element);
+            }
+
+            command = scanner.nextLine();
+        }
+
+        for (int number : numbers) {
+            System.out.print(number + " ");
+        }
+    }
+}
