100760826fd86bb58097d514f7bad8bfd5f84f92
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2023-11-28 12:01:48 +0200

    Add files via upload

---
+++ Java-Fund-Sept-2023/07. Objects and Classes/Lab/BigFactorial_03.java
@@ -0,0 +1,21 @@
+package objects_and_classes;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BigFactorial_03 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        BigInteger result = BigInteger.valueOf(1);
+
+        for (int i = 1; i <= n; i++) {
+            result = result.multiply(BigInteger.valueOf(i));
+        }
+
+        System.out.println(result);
+    }
+}
