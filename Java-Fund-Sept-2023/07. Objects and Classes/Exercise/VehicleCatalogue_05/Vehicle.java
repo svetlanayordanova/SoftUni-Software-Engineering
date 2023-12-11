da498dc621fb15ab9a04f2cd0dcb47b0b97f0b84
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2023-12-11 15:26:40 +0200

    Add files via upload

---
+++ Java-Fund-Sept-2023/07. Objects and Classes/Exercise/VehicleCatalogue_05/Vehicle.java
@@ -0,0 +1,55 @@
+package objects_and_classes_exercise.VehicleCatalogue_05;
+
+public class Vehicle {
+    private String type;
+    private String model;
+    private String color;
+    private int horsepower;
+
+    public Vehicle(String type, String model, String color, int horsepower) {
+        this.type = type;
+        this.model = model;
+        this.color = color;
+        this.horsepower = horsepower;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getHorsepower() {
+        return this.horsepower;
+    }
+
+    public void setHorsepower(int horsepower) {
+        this.horsepower = horsepower;
+    }
+
+    public String toString() {
+        String typeVeh = type.equals("car") ? "Car" : "Truck";
+        return "Type: " + typeVeh + "\n" +
+                "Model: " + model + "\n" +
+                "Color: " + color + "\n" +
+                "Horsepower: " + horsepower;
+    }
+}
