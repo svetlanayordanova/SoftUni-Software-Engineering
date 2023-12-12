236cc09dbb3d7d6a51fafa5ec863bd666ae15d9d
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2023-12-12 14:49:20 +0200

    Add files via upload

---
+++ Java-Fund-Sept-2023/08. Maps, Lambda and StreamAPI/Lab/WordFilter_04.java
@@ -0,0 +1,19 @@
+package maps_lambda_and_streamAPI;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class WordFilter_04 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] words = Arrays.stream(scanner.nextLine().split(" "))
+                .filter(word -> word.length() % 2 == 0)
+                .toArray(String[]::new);
+
+        for (String word : words) {
+            System.out.println(word);
+        }
+    }
+}
