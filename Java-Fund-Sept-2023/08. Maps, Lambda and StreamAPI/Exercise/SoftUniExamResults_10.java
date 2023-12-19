f53d0263752fb08b307ee4757b96738df1168e77
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2023-12-19 18:53:55 +0200

    Add files via upload

---
+++ Java-Fund-Sept-2023/08. Maps, Lambda and StreamAPI/Exercise/SoftUniExamResults_10.java
@@ -0,0 +1,54 @@
+package maps_lambda_and_streamAPI_exercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class SoftUniExamResults_10 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Map<String, Integer> userPoints = new LinkedHashMap<>();
+        Map<String, Integer> languageCount = new LinkedHashMap<>();
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("exam finished")) {
+            String[] splitArray = input.split("-");
+            String username = splitArray[0];
+
+            if (splitArray.length == 3) {
+                String language = splitArray[1];
+                int points = Integer.parseInt(splitArray[2]);
+
+                if (!userPoints.containsKey(username)) {
+                    userPoints.put(username, points);
+                } else {
+                    int currentPoints = userPoints.get(username);
+                    if (points > currentPoints) {
+                        userPoints.put(username, points);
+                    }
+                }
+
+                if (!languageCount.containsKey(language)) {
+                    languageCount.put(language, 1);
+                } else {
+                    languageCount.put(language, languageCount.get(language) + 1);
+                }
+            } else {
+                userPoints.remove(username);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        System.out.println("Results:");
+        userPoints.entrySet()
+                .forEach(e -> System.out.println(e.getKey() + " | " + e.getValue()));
+
+        System.out.println("Submissions:");
+        languageCount.entrySet()
+                .forEach(e -> System.out.println(e.getKey() + " - " + e.getValue()));
+    }
+}
