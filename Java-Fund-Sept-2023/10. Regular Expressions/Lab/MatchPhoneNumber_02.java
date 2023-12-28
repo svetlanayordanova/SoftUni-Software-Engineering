ea127428079d9f4afdaf3531c98d5582b57576b8
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2023-12-28 13:36:01 +0200

    Add files via upload

---
+++ Java-Fund-Sept-2023/10. Regular Expressions/Lab/MatchPhoneNumber_02.java
@@ -0,0 +1,27 @@
+package regular_expressions;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MatchPhoneNumber_02 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile("\\+359([ -])2\\1[0-9]{3}\\1[0-9]{4}\\b");
+        Matcher matcher = pattern.matcher(text);
+
+        List<String> matches = new ArrayList<>();
+
+        while (matcher.find()) {
+            matches.add(matcher.group());
+        }
+
+        System.out.println(String.join(", ", matches));
+    }
+}
