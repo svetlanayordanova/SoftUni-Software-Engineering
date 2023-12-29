522ed0edc2b53777e2a2f3d15847eda61e28da51
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2023-12-29 15:50:28 +0200

    Add files via upload

---
+++ Java-Fund-Sept-2023/10. Regular Expressions/Exercise/ValidatePassword_06.java
@@ -0,0 +1,42 @@
+package regular_expressions_exercise;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidatePassword_06 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <= n; i++) {
+            String password = scanner.nextLine();
+
+            String regexPassword = "_\\.+(?<passwordText>[A-Z][A-Za-z0-9]{4,}[A-Z])_\\.+";
+
+            Pattern pattern = Pattern.compile(regexPassword);
+            Matcher matcher = pattern.matcher(password);
+
+            if (matcher.find()) {
+                String textPassword = matcher.group("passwordText");
+                StringBuilder sbDigitsCategory = new StringBuilder();
+
+                for (char symbol : textPassword.toCharArray()) {
+                    if (Character.isDigit(symbol)) {
+                        sbDigitsCategory.append(symbol);
+                    }
+                }
+
+                if (sbDigitsCategory.length() == 0) {
+                    System.out.println("Group: default");
+                } else {
+                    System.out.println("Group: " + sbDigitsCategory);
+                }
+            } else {
+                System.out.println("Invalid pass!");
+            }
+        }
+    }
+}
