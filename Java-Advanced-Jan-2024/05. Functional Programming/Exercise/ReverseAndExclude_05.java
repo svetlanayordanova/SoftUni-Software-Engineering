29ec9b7d1d812092148e2881c9621fecc21555fd
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-02-20 17:43:16 +0200

    Add files via upload

---
+++ Java-Advanced-Jan-2024/05. Functional Programming/Exercise/ReverseAndExclude_05.java
@@ -0,0 +1,29 @@
+package functional_programming_exercise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class ReverseAndExclude_05 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Predicate<Integer> notDivisibleByN = number -> number % n != 0;
+
+        Collections.reverse(numbers);
+
+        numbers.stream()
+                .filter(notDivisibleByN)
+                .forEach(num -> System.out.print(num + " "));
+    }
+}
