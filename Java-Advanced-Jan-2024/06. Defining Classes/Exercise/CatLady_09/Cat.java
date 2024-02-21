ee0b83c4d399e2467228c83c914c4c318e541edb
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-02-21 13:24:04 +0200

    Add files via upload

---
+++ Java-Advanced-Jan-2024/06. Defining Classes/Exercise/CatLady_09/Cat.java
@@ -0,0 +1,19 @@
+package defining_classes_exercise.CatLady_09;
+
+public class Cat {
+    private String name;
+    private String type;
+
+    public Cat(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
