2f2ab872abaeb566a8074cbe4577a29dfd86e330
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-02-22 12:50:22 +0200

    Add files via upload

---
+++ Java-Advanced-Jan-2024/07. Generics/Exercise/CustomList_07/Main.java
@@ -0,0 +1,48 @@
+package generics_exercise.CustomList_07;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+        CustomList<String> customList = new CustomList<>();
+
+        while (!"END".equals(line)) {
+
+            String[] tokens = line.split("\\s+");
+            String command = tokens[0];
+
+            switch (command) {
+                case "Add":
+                    customList.add(tokens[1]);
+                    break;
+                case "Remove":
+                    customList.remove(Integer.parseInt(tokens[1]));
+                    break;
+                case "Contains":
+                    System.out.println(customList.contains(tokens[1]));
+                    break;
+                case "Swap":
+                    customList.swap(Integer.parseInt(tokens[1]), Integer.parseInt(tokens[2]));
+                    break;
+                case "Greater":
+                    System.out.println(customList.countGreaterThan(tokens[1]));
+                    break;
+                case "Max":
+                    System.out.println(customList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(customList.getMin());
+                    break;
+                case "Print":
+                    customList.forEach(System.out::println);
+                    break;
+            }
+
+            line = scanner.nextLine();
+        }
+    }
+}
