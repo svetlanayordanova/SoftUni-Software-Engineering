700a7e40169ae52a0c897b9dec774786e3b808b3
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-02-26 13:06:49 +0200

    Add files via upload

---
+++ Java-Advanced-Jan-2024/08. Iterators and Comparators/Lab/Book_01/Main.java
@@ -0,0 +1,23 @@
+package iterators_and_comparators.Book_01;
+
+import iterators_and_comparators.Book_01.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        Book bookThree = new Book("The Documents in the Case", 2002);
+        Book bookTwo = new Book("The Documents in the Case", 1930, "Dorothy Sayers", "Robert Eustace");
+
+        List<Book> books = new ArrayList<>();
+        books.add(bookOne);
+        books.add(bookTwo);
+        books.add(bookThree);
+    }
+}
