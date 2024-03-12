b65429381e1834b4e58059c05eb748ebce73b676
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-12 09:23:39 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/01. Working with Abstraction/Exercise/CardsWithPower_03/Main.java
@@ -0,0 +1,19 @@
+package working_with_abstraction_exercise.CardsWithPower_03;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String cardRank = scanner.nextLine();
+        String cardSuit = scanner.nextLine();
+
+        CardRank cardRankEnum = CardRank.valueOf(cardRank);
+        CardSuit cardSuitEnum = CardSuit.valueOf(cardSuit);
+
+        Card card = new Card(cardRankEnum, cardSuitEnum);
+        card.print();
+    }
+}
