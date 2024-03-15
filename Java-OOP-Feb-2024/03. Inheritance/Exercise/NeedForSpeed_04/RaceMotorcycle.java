916e63824c0013494e63acaf353b77d5c8e1ac7e
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:11:38 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/NeedForSpeed_04/RaceMotorcycle.java
@@ -0,0 +1,10 @@
+package inheritance_exercise.NeedForSpeed_04;
+
+public class RaceMotorcycle extends Motorcycle {
+    private static final double DEFAULT_FUEL_CONSUMPTION = 8;
+
+    public RaceMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        super.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+}
