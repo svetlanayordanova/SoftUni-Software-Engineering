49e8bae90cbae7483bb7338db96eeb538e802600
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:17:09 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/PlayersAndMonsters_03/MuseElf.java
@@ -0,0 +1,7 @@
+package inheritance_exercise.PlayersAndMonsters_03;
+
+public class MuseElf extends Elf {
+    public MuseElf(String username, int level) {
+        super(username, level);
+    }
+}
