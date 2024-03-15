202d34df6463cdd5eebb05ddb0ca2ce8b12bf19a
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:10:05 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/NeedForSpeed_04/CrossMotorcycle.java
@@ -0,0 +1,7 @@
+package inheritance_exercise.NeedForSpeed_04;
+
+public class CrossMotorcycle extends Motorcycle {
+    public CrossMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
