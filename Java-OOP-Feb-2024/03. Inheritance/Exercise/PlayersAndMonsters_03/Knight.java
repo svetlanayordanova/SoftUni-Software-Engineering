ecac512dea626eb50034af9cea7bdcb98424fee6
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:16:20 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/PlayersAndMonsters_03/Knight.java
@@ -0,0 +1,8 @@
+package inheritance_exercise.PlayersAndMonsters_03;
+
+public class Knight extends Hero {
+
+    public Knight(String username, int level) {
+        super(username, level);
+    }
+}
