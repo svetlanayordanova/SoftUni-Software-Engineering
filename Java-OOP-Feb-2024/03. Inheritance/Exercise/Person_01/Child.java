b2492017b0f8c93bb43e0f12181625611e31be6b
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:12:58 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/Person_01/Child.java
@@ -0,0 +1,9 @@
+package inheritance_exercise.Person_01;
+
+import inheritance_exercise.Person_01.Person;
+
+public class Child extends Person {
+    public Child(String name, int age) {
+        super(name, age);
+    }
+}
