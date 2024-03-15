3433e3d02a73b413d241c920fcd1f91caf0b9dc2
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 15:06:52 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/Zoo_02/Animal.java
@@ -0,0 +1,13 @@
+package inheritance_exercise.Zoo_02;
+
+public class Animal {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
