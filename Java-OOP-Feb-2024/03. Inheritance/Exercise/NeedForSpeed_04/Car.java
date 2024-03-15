41473e61b6eeb52df1b96ad5ac44f5bb94093198
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:09:46 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/NeedForSpeed_04/Car.java
@@ -0,0 +1,10 @@
+package inheritance_exercise.NeedForSpeed_04;
+
+public class Car extends Vehicle {
+    private static final double DEFAULT_FUEL_CONSUMPTION = 3;
+
+    public Car(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        super.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+}
