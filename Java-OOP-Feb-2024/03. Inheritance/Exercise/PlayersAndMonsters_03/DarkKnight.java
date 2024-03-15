da518cd6c27a1a9ece401236814500219e5dc64f
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:14:55 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/PlayersAndMonsters_03/DarkKnight.java
@@ -0,0 +1,7 @@
+package inheritance_exercise.PlayersAndMonsters_03;
+
+public class DarkKnight extends Knight {
+    public DarkKnight(String username, int level) {
+        super(username, level);
+    }
+}
