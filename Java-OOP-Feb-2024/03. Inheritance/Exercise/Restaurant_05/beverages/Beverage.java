fd697d359d5d4fc618f1bcc2a9799f5e06c98aec
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:19:49 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/Restaurant_05/beverages/Beverage.java
@@ -0,0 +1,17 @@
+package inheritance_exercise.Restaurant_05.beverages;
+
+import inheritance_exercise.Restaurant_05.Product;
+
+import java.math.BigDecimal;
+
+public class Beverage extends Product {
+    private double milliliters;
+    public Beverage(String name, BigDecimal price, double milliliters) {
+        super(name, price);
+        this.milliliters = milliliters;
+    }
+
+    public double getMilliliters() {
+        return milliliters;
+    }
+}
