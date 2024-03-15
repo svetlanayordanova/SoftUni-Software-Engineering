ec7be458c5fe25903a9b82aa030fccff27883e45
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:04:04 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Lab/SingleInheritance_01/Main.java
@@ -0,0 +1,10 @@
+package inheritance.SingleInheritance_01;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
