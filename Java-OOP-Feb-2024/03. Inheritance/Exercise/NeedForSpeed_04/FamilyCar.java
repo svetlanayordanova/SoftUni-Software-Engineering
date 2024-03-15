08f309f780739964fffcba7756d6ea0708e7e525
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:10:31 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/NeedForSpeed_04/FamilyCar.java
@@ -0,0 +1,7 @@
+package inheritance_exercise.NeedForSpeed_04;
+
+public class FamilyCar extends Car {
+    public FamilyCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
