c0ee4e686882d2ba00395c30e98b1e43f27e21e3
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:21:57 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/Restaurant_05/beverages/hot_beverage/Tea.java
@@ -0,0 +1,9 @@
+package inheritance_exercise.Restaurant_05.beverages.hot_beverage;
+
+import java.math.BigDecimal;
+
+public class Tea extends HotBeverage {
+    public Tea(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
