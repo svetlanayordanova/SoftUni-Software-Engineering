08f0a77482ef9bfd5c6ebd5b52a358d9b6d009e2
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:08:32 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/Animals_06/Main.java
@@ -0,0 +1,50 @@
+package inheritance_exercise.Animals_06;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String line = scanner.nextLine();
+
+        while (!"Beast!".equals(line)) {
+
+            try {
+                switch (line) {
+                    case "polymorphism.Animals_03.Cat":
+                        String[] tokens = scanner.nextLine().split("\\s+");
+                        Cat cat = new Cat(tokens[0], Integer.parseInt(tokens[1]), tokens[2]);
+                        System.out.println(cat);
+                        break;
+                    case "polymorphism.Animals_03.Dog":
+                        tokens = scanner.nextLine().split("\\s+");
+                        Dog dog = new Dog(tokens[0], Integer.parseInt(tokens[1]), tokens[2]);
+                        System.out.println(dog);
+                        break;
+                    case "Frog":
+                        tokens = scanner.nextLine().split("\\s+");
+                        Frog frog = new Frog(tokens[0], Integer.parseInt(tokens[1]), tokens[2]);
+                        System.out.println(frog);
+                        break;
+                    case "Kitten":
+                        tokens = scanner.nextLine().split("\\s+");
+                        Kitten kitten = new Kitten(tokens[0], Integer.parseInt(tokens[1]));
+                        System.out.println(kitten);
+                        break;
+                    case "Tomcat":
+                        tokens = scanner.nextLine().split("\\s+");
+                        Tomcat tomcat = new Tomcat(tokens[0], Integer.parseInt(tokens[1]));
+                        System.out.println(tomcat);
+                        break;
+                }
+
+            } catch (IllegalArgumentException ex) {
+                System.out.println(ex.getMessage());
+            }
+
+            line = scanner.nextLine();
+        }
+    }
+}
