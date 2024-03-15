8b6f8660013e4afd03a29716bec62d494e7babd4
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:06:51 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/Animals_06/Cat.java
@@ -0,0 +1,12 @@
+package inheritance_exercise.Animals_06;
+
+public class Cat extends Animal {
+    public Cat(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+
+    @Override
+    public String produceSound() {
+        return "Meow meow";
+    }
+}
