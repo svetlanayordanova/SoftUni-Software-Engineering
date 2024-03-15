fffd726c5dd09498a2dd3caba1d0536813b0881a
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:26:42 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/Restaurant_05/food/starter/Soup.java
@@ -0,0 +1,9 @@
+package inheritance_exercise.Restaurant_05.food.starter;
+
+import java.math.BigDecimal;
+
+public class Soup extends Starter {
+    public Soup(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+    }
+}
