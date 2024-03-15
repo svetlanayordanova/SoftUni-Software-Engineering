9291e5cc8408f871cc8548053d66ed44e58f6f33
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:11:18 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/NeedForSpeed_04/Motorcycle.java
@@ -0,0 +1,7 @@
+package inheritance_exercise.NeedForSpeed_04;
+
+public class Motorcycle extends Vehicle {
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
