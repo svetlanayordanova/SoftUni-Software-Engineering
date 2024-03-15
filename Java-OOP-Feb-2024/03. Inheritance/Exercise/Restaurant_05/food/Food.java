52c82dcb4af44e7c18ec5ffe77b7666368049ee0
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:23:56 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/Restaurant_05/food/Food.java
@@ -0,0 +1,18 @@
+package inheritance_exercise.Restaurant_05.food;
+
+import inheritance_exercise.Restaurant_05.Product;
+
+import java.math.BigDecimal;
+
+public class Food extends Product {
+    private double grams;
+
+    public Food(String name, BigDecimal price, double grams) {
+        super(name, price);
+        this.grams = grams;
+    }
+
+    public double getGrams() {
+        return grams;
+    }
+}
