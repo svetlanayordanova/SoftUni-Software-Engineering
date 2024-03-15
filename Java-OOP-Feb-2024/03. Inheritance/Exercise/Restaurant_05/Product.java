ab66624877d09ca4bb53223ec303562e6ddabfc0
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-15 14:28:02 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/03. Inheritance/Exercise/Restaurant_05/Product.java
@@ -0,0 +1,21 @@
+package inheritance_exercise.Restaurant_05;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private String name;
+    private BigDecimal price;
+
+    public Product(String name, BigDecimal price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+}
