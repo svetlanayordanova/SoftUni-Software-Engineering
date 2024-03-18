620416d6df04165036a92dc8c6d4b184425fd8fb
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-18 11:17:08 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/04. Interfaces and Abstraction/Exercise/MultipleImplementation_02/Identifiable.java
@@ -0,0 +1,5 @@
+package interfaces_and_abstraction_exercise.MultipleImplementation_02;
+
+public interface Identifiable {
+    String getId();
+}
---
+++ Java-OOP-Feb-2024/04. Interfaces and Abstraction/Exercise/MultipleImplementation_02/Person.java
@@ -0,0 +1,7 @@
+package interfaces_and_abstraction_exercise.MultipleImplementation_02;
+
+public interface Person {
+    String getName();
+
+    int getAge();
+}
