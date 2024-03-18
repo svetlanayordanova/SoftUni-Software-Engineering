10d9b52997653955fcf201d9d2a7576fe10a98a5
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-18 10:52:06 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/04. Interfaces and Abstraction/Exercise/BirthdayCelebrations_03/Pet.java
@@ -0,0 +1,20 @@
+package interfaces_and_abstraction_exercise.BirthdayCelebrations_03;
+
+public class Pet implements Birthable {
+    private String name;
+    private String birthDate;
+
+    public Pet(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String getBirthDate() {
+        return this.birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
