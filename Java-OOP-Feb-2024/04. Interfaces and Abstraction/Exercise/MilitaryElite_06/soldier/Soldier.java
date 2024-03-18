61b80d8daef9e3645e1b0db43d01900ffcab1f34
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-18 11:14:32 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/04. Interfaces and Abstraction/Exercise/MilitaryElite_06/soldier/Soldier.java
@@ -0,0 +1,7 @@
+package interfaces_and_abstraction_exercise.MilitaryElite_06.soldier;
+
+public interface Soldier {
+    int getId();
+    String getFirstName();
+    String getLastName();
+}
---
+++ Java-OOP-Feb-2024/04. Interfaces and Abstraction/Exercise/MilitaryElite_06/soldier/SpecialisedSoldier.java
@@ -0,0 +1,7 @@
+package interfaces_and_abstraction_exercise.MilitaryElite_06.soldier;
+
+import interfaces_and_abstraction_exercise.MilitaryElite_06.enumm.Corp;
+
+public interface SpecialisedSoldier {
+    Corp getCorps(); // "Airforces" or "Marines" (Enumeration)
+}
