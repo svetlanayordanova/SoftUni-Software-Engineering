ccc595c76add8bd18dbb7591b716123130f84f95
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-19 11:36:51 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/05. Polymorphism/Exercise/Word_03/CutTransform.java
@@ -0,0 +1,15 @@
+package polymorphism_exercise.Word_03;
+
+public class CutTransform implements TextTransform {
+    private StringBuilder lastCut = new StringBuilder();
+    @Override
+    public void invokeOn(StringBuilder text, int startIndex, int endIndex) {
+        this.lastCut = new StringBuilder();
+        this.lastCut.append(text.substring(startIndex, endIndex));
+        text.delete(startIndex, endIndex);
+    }
+
+    public StringBuilder getLastCut() {
+        return lastCut;
+    }
+}
