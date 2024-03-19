193f261550cd3e9040fab840a08c3434a89d68cb
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-19 11:32:24 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/05. Polymorphism/Exercise/VehiclesExtension_02/Car.java
@@ -0,0 +1,7 @@
+package polymorphism_exercise.VehiclesExtension_02;
+
+public class Car extends VehicleImpl {
+    public Car(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption + 0.9, tankCapacity);
+    }
+}
