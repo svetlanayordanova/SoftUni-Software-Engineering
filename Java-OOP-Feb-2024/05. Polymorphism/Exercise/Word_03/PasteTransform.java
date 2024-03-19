e0860218c07483cf09aff13851dbc476b7f45834
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-19 11:37:53 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/05. Polymorphism/Exercise/Word_03/PasteTransform.java
@@ -0,0 +1,15 @@
+package polymorphism_exercise.Word_03;
+
+public class PasteTransform implements TextTransform {
+    private final CutTransform cutTransform;
+
+    public PasteTransform(CutTransform cutTransform) {
+        this.cutTransform = cutTransform;
+    }
+
+    @Override
+    public void invokeOn(StringBuilder text, int startIndex, int endIndex) {
+
+        text.replace(startIndex, endIndex, this.cutTransform.getLastCut().toString());
+    }
+}
