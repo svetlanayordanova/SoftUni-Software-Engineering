fc2fee26a4741ddde417f6dc0cef02b1486f7ebb
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-03-19 11:17:51 +0200

    Add files via upload

---
+++ Java-OOP-Feb-2024/05. Polymorphism/Lab/Animals_03/Cat.java
@@ -0,0 +1,14 @@
+package polymorphism.Animals_03;
+
+public class Cat extends Animal {
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    protected String explainSelf() {
+        return String.format("I am %s and my favourite food is %s MEEOW",
+                super.getName(),
+                super.getFavouriteFood());
+    }
+}
