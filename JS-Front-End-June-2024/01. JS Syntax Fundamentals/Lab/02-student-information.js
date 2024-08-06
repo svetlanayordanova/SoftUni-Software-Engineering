afb12c2d6d25dad92544447d7252315c8df6602d
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-08-06 08:52:10 +0300

    Add files via upload

---
+++ JS-Front-End-June-2024/01. JS Syntax Fundamentals/Lab/02-student-information.js
@@ -0,0 +1,7 @@
+function printInfo(name, age, averageGrade) {
+    let info = `Name: ${name}, Age: ${age}, Grade: ${averageGrade.toFixed(2)}`;
+
+    console.log(info);
+}
+
+printInfo('Marry', 12, 6.00);
