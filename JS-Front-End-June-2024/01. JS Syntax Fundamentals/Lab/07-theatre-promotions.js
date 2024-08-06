f182575fdd5265a61b1eaf5db691cd0067fa2f91
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-08-06 08:54:04 +0300

    Add files via upload

---
+++ JS-Front-End-June-2024/01. JS Syntax Fundamentals/Lab/07-theatre-promotions.js
@@ -0,0 +1,39 @@
+function calculatePrice(day, age) {
+    let price;
+
+    switch(day) {
+        case 'Weekday':
+            if ((age >= 0 && age <= 18) || (age > 64 && age <= 122)) {
+                price = '12$';
+            } else if (age > 18 && age <= 64) {
+                price = '18$';
+            } else {
+                price = 'Error!';
+            }
+        break;
+        case 'Weekend':
+            if ((age >= 0 && age <= 18) || (age > 64 && age <= 122)) {
+                price = '15$';
+            } else if (age > 18 && age <= 64) {
+                price = '20$';
+            } else {
+                price = 'Error!';
+            }
+        break;
+        case 'Holiday':
+            if (age >= 0 && age <= 18) {
+                price = '5$';
+            } else if (age > 18 && age <= 64) {
+                price = '12$';
+            } else if (age > 64 && age <= 122) {
+                price = '10$';
+            } else {
+                price = 'Error!';
+            }
+        break;
+    }
+
+    console.log(`${price}`);
+}
+
+calculatePrice('Holiday', 15);
