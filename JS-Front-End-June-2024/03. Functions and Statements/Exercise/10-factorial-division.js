7015cf1d17ac09c64ba932a3f1bd16cec13fab6e
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-08-14 08:19:25 +0300

    Add files via upload

---
+++ JS-Front-End-June-2024/03. Functions and Statements/Exercise/10-factorial-division.js
@@ -0,0 +1,20 @@
+function factorialDivision(firstNumber, secondNumber) {
+    const findFactorial = (n) => {
+        if (n < 0) {
+            return -1;
+        }
+
+        if (n === 0) {
+            return 1;
+        }
+
+        return n * findFactorial(n - 1);
+    };
+
+    const result = findFactorial(firstNumber) / findFactorial(secondNumber);
+
+    console.log(result.toFixed(2));
+}
+
+factorialDivision(5, 2);
+factorialDivision(6, 2);
