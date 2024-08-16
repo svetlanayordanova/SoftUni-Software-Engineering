01386da7647bcdb3a28e12842bbd7373810bc22d
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-08-16 08:37:31 +0300

    Add files via upload

---
+++ JS-Front-End-June-2024/04. Objects and Classes/Lab/03-convert-to-object.js
@@ -0,0 +1,10 @@
+function convertToObject(jsonStr) {
+    let person = JSON.parse(jsonStr);
+
+    for (const key in person) {
+        console.log(`${key}: ${person[key]}`);
+    }
+}
+
+convertToObject('{"name": "George", "age": 40, "town": "Sofia"}');
+convertToObject('{"name": "Peter", "age": 35, "town": "Plovdiv"}')
