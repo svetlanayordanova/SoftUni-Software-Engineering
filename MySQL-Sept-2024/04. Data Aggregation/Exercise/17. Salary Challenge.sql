0a7abaea20e958ff2351f9db97117d4d58328875
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-09-26 08:40:00 +0300

    Add files via upload

---
+++ MySQL-Sept-2024/04. Data Aggregation/Exercise/17. Salary Challenge.sql
@@ -0,0 +1,10 @@
+SELECT
+	e.first_name,
+    e.last_name,
+    e.department_id
+FROM employees AS e
+WHERE salary > (SELECT AVG(salary) 
+                FROM employees
+                WHERE department_id = e.department_id)
+ORDER BY e.department_id, e.employee_id
+LIMIT 10;
