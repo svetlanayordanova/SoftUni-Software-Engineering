59b0ffb2696546b7a91f04a4548f5294e18ac4af
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-10-03 09:12:58 +0300

    Add files via upload

---
+++ MySQL-Sept-2024/07. Database Programmability and Transactions/Lab/04. Triggered.sql
@@ -0,0 +1,26 @@
+CREATE TABLE deleted_employees(
+	employee_id INT PRIMARY KEY AUTO_INCREMENT,
+	first_name VARCHAR(50),
+    last_name VARCHAR(50),
+	middle_name VARCHAR(50),
+    job_title VARCHAR(50),
+    department_id INT,
+    salary DECIMAL(19,4)
+);
+    
+DELIMITER $$
+CREATE TRIGGER tr_deleted_employees
+AFTER DELETE
+ON employees
+FOR EACH ROW
+BEGIN
+	INSERT INTO deleted_employees
+		(first_name, middle_name, last_name, job_title, department_id, salary)
+		VALUES(
+			OLD.first_name, 
+			OLD.middle_name, 
+			OLD.last_name,
+			OLD.job_title,
+			OLD.department_id,
+			OLD.salary);
+END $$
