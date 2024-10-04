5f86413da12bffcacb2c1e92766d5b43db18106f
svetlanayordanova <145579663+svetlanayordanova@users.noreply.github.com>
2024-10-04 08:44:17 +0300

    Add files via upload

---
+++ MySQL-Sept-2024/07. Database Programmability and Transactions/Exercise/16. Emails Trigger.sql
@@ -0,0 +1,21 @@
+CREATE TABLE notification_emails(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    recipient INT,
+    subject VARCHAR(255),
+    body TEXT
+);
+
+DELIMITER $$
+CREATE TRIGGER tr_logs_insert
+AFTER INSERT
+ON logs
+FOR EACH ROW
+BEGIN
+	INSERT INTO notification_emails(recipient, subject, body)
+    VALUES(
+			NEW.account_id,
+            CONCAT('Balance change for account: ', NEW.account_id),
+            CONCAT('On ', DATE_FORMAT(NOW(), '%b %d %Y'), ' at ',
+			DATE_FORMAT(NOW(), '%r'), ' your balance was changed from ',
+			ROUND(NEW.old_sum, 0), ' to ', ROUND(NEW.new_sum, 0), '.'));
+END $$
